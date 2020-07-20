4fc313349920bc682cae6eb5f8cd04e00049f65c
TGion <tobias@gion.io>
2020-07-20 09:30:51 +0200

    Added some comments to try out git commits

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::env;
 
+// main function
 fn main() {
     let args: Vec<String> = env::args().collect();
+
+	// Print all arguments on the console
 	println!("Arguments are {:?}", args);
 }
